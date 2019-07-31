9c930b93ad4a9d28935e44f9510bfee9eeb73a27
Maxim <makalexs@mail.ru>
2019-07-31 09:00:20 +0300

    ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -64,6 +64,21 @@ resource "google_compute_firewall" "firewall_puma" {
   target_tags = ["reddit-app"]
 }
 
+resource "google_compute_firewall" "firewall_puma_http" {
+  name = "allow-puma-http-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
+
 resource "google_compute_address" "app_ip" {
 	name 	= "reddit-app-ip"
 	region 	= "${var.region}"
